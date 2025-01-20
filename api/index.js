0b6c10055b11569996e6cb4d85d1fafacf163a69
Leonardo <leorocha11@outlook.com>
2025-01-20 16:56:36 -0300

    cors url

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Configuração do CORS para aceitar requisições apenas do localhost
 const corsOptions = {
-  origin: "https://controlefinanceiro-dktx.onrender.com",  // URL do frontend local (Vite)
+  origin: "https://controlefinanceiro-1.onrender.com",  // URL do frontend local (Vite)
   credentials: true,  // Se você estiver usando cookies
 };
 
