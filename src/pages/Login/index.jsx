c2017c6d808d3eef7799e57b536a06988c0dfa17
Leonardo <leorocha11@outlook.com>
2025-01-20 11:14:26 -0300

    login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -19,7 +19,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://controle-financeiro-hazel-kappa.vercel.app/api/usuarios/login", {
+    const response = await fetch("https://controle-financeiro-hazel-kappa.vercel.app/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
