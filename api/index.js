90d0ec77a91a089f5410ac49bf926207c06be651
Leonardo <leorocha11@outlook.com>
2025-02-11 15:52:21 -0300

    cors

--- api/index.js
+++ api/index.js
@@ -2,28 +2,25 @@ import express from "express";
 import cors from "cors";
 import gastosRouter from "./routes/gastos.js";
 import usuariosRouter from "./routes/usuarios.js";
-
 import loginRouter from "./routes/logins.js";
-
 import registerRouter from "./routes/registers.js";
 import requestPasswordRouter from "./routes/requestPassword.js";
 import resetPasswordRouter from "./routes/resetPassword.js";
 import dotenv from "dotenv";
-
 import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
 
-// Configuração do CORS para aceitar requisições apenas do localhost
+// Configuração do CORS para aceitar requisições de múltiplos domínios
 const corsOptions = {
   origin: [
-    "https://controle-financeiro-ashen.vercel.app",
-    "https://controlefinanceiro-1.onrender.com",
-    "https://controlefinanceiro-dktx.onrender.com",
-    "http://localhost:5173",
-  ], // URL do frontend local (Vite)
+    "https://controle-financeiro-ashen.vercel.app", // Frontend no Vercel
+    "https://controlefinanceiro-1.onrender.com", // Frontend no Render
+    "https://controlefinanceiro-dktx.onrender.com", // Outro domínio no Render
+    "http://localhost:5173", // Frontend local durante o desenvolvimento
+  ],
   credentials: true, // Se você estiver usando cookies
 };
 
@@ -33,21 +30,19 @@ app.use(express.json());
 
 // Definindo as rotas
 app.use("/gastos", gastosRouter);
-
 app.use("/login", loginRouter);
-
 app.use("/register", registerRouter);
 app.use("/usuarios", usuariosRouter);
 app.use("/requestPassword", requestPasswordRouter);
 app.use("/resetPassword", resetPasswordRouter);
 
 // Definindo a porta do servidor
-/* const PORT = process.env.PORT || 8801;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-}); */
-
+const PORT = process.env.PORT || 8801; // Usa a variável de ambiente PORT ou o valor padrão 8801
 const host = process.env.HOST || "localhost"; // Definindo a URL com base no ambiente
 console.log(`Servidor rodando em http://${host}:${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
 
 export default app;
