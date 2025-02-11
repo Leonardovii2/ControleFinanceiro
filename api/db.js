83486605cb76bf3a37f76acd8af2b1a3158c55c1
Leonardo <leorocha11@outlook.com>
2025-02-11 14:12:48 -0300

    config db render

--- api/db.js
+++ api/db.js
@@ -1,12 +1,23 @@
-import pkg from 'pg';
+import pkg from "pg";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { Pool } = pkg;
 
 export const db = new Pool({
-  host: "localhost",
+  /* host: "localhost",
   port: 5432,
   user: "postgres",
   password: "bancoleo12",
-  database: "crud",
+  database: "crud", */
+
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 /* const db = new Pool({
